59ca2f72c2ea853d47f32a169be90129429dd107
carlospalomino <carlospalomino@outlook.com>
2022-07-01 21:41:13 -0500

    Tarea semana 4 terminada

--- 3.-ExploratoryDataAnalysis/TareaSemana4/plot6.R
+++ 3.-ExploratoryDataAnalysis/TareaSemana4/plot6.R
@@ -8,16 +8,20 @@ setwd("/Users/carlospalominovidal/Library/CloudStorage/OneDrive-UniversidadPerua
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-png(filename="plot5.png", width=480, height = 480 )
+png(filename="plot6.png", width=480, height = 480 )
 
-baltimore<-NEI[(NEI$fips=="24510" ),]
+baltimore<-NEI[(NEI$fips=="24510" | NEI$fips=="06037" ),]
 
 vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
 vehiclesSCC <- SCC[vehicles,]$SCC
 vehiclesNEI <- baltimore[baltimore$SCC %in% vehiclesSCC,]
 
-boxplot(log10(Emissions)~year,vehiclesNEI,xlab="Anio",ylab="Emissions")      
-title("Emisiones Baltimore Vehicules related")
+
+
+
+
+x<-qplot(year,log10(Emissions),data=vehiclesNEI,color=fips,facets=fips~.,xlab="YEARS",ylab="EMISSIONS PM2.5",main = "BALTIMORE VS LA EVOLUTION")
+print(x)
 
 #motor<-SCC  %>% filter_all(any_vars(. %in% c("Motor")))
 #baltimormotordata<-baltimore[baltimore$SCC %in% coal$SCC,]
